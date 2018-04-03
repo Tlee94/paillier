c9470c064b8a34a07ca5fcc309ef7aba207e6325
Timothy Lee <tlee9400@gmail.com>
2018-04-03 14:03:51 -0400

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+//To compile use g++ test1.cpp -o test1 -std=c++11
+//execute ./test1
+int main()
+{
+    string str = " maybe 3 * 6 2578*20+";
+
+    // Tokens
+    vector<pair <string, string>> v
+    {
+        {"[0-9]+" , "NUMBERS"} ,
+        {"[a-z]+" , "IDENTIFIERS"},
+        {"\\*|\\+", "OPERATORS"}
+        //still need store and load
+    };
+
+    string reg;
+
+    for(const auto x : v)
+      reg += "(" + x.first + ")|";
+
+    reg.pop_back();
+
+    cout << "Tokens: " << reg << endl;
+
+    regex re(reg);
+
+    sregex_iterator words_begin = sregex_iterator(str.begin(), str.end(), re);
+    sregex_iterator words_end = sregex_iterator();
+
+    for(sregex_iterator i = words_begin; i != words_end; i++)
+    {
+        size_t index = 0;
+
+        for(index = 0; index < i->size(); ++index)
+            if(!i->str(index + 1).empty())
+                break;
+
+        cout << i->str() << " " << v[index].second << endl;
+    }
+    return 0;
+}
